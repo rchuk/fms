49fb19baa1f083d9caa38775ad4467fe784c4e60
W1nLin4n <grechkaartema@gmail.com>
2024-06-05 16:39:44 +0300

    Baseline for authentication pages

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+</script>
+
+<h1>Welcome to FMS</h1>
+<div>
+	<button on:click={() => goto('/auth/login')}>Login</button>
+	<button on:click={() => goto('/auth/register')}>Register</button>
+</div>

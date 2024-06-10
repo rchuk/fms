8d0108752085b635d96252a5a31e1a61146d98ff
Ruslan Omelchuk <romelchuk12@gmail.com>
2024-06-10 20:32:37 +0300

    Fixed class name

--- frontend/src/app/organizations/page.tsx
+++ frontend/src/app/organizations/page.tsx
@@ -1,7 +1,7 @@
 import {Box} from "@mui/material";
 import OrganizationList from "@/lib/components/organization/OrganizationList";
 
-export default function UserWorkspacesPage() {
+export default function UserOrganizationsPage() {
   return (
     <Box display="flex" flexDirection="column" height="100%" padding={2} boxSizing="border-box">
       <OrganizationList />

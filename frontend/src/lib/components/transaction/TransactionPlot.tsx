a2b7a47ed7452f1d1fc0128dd87bc7f1b059a9fc
Ruslan Omelchuk <romelchuk12@gmail.com>
2024-06-13 22:08:12 +0300

    Added colors to the transaction plot

--- frontend/src/lib/components/transaction/TransactionPlot.tsx
+++ frontend/src/lib/components/transaction/TransactionPlot.tsx
@@ -1,5 +1,6 @@
 import {PieChart} from "@mui/x-charts";
 import {TransactionResponse} from "../../../../generated";
+import {PieValueType} from "@mui/x-charts";
 
 
 type TransactionPlotProps = {
@@ -9,13 +10,14 @@ type TransactionPlotProps = {
 export default function TransactionPlot(props: TransactionPlotProps) {
   // TODO: Add different chart types
 
-  function mapItem(item: TransactionResponse) {
+  function mapItem(item: TransactionResponse): PieValueType {
     return {
       id: item.id,
       value: Math.abs(item.amount),
-      label: item.category.name
-    }
-  };
+      label: item.category.name,
+      color: `#${item.category.uiColor}`
+    };
+  }
 
   return (
     <PieChart

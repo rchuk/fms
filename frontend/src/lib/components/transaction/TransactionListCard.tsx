01b56e17e4315856261716fa6cd94dd4619e2e40
Ruslan Omelchuk <romelchuk12@gmail.com>
2024-06-14 23:21:28 +0300

    Added colored circles to transactions and categories

--- frontend/src/lib/components/transaction/TransactionListCard.tsx
+++ frontend/src/lib/components/transaction/TransactionListCard.tsx
@@ -2,6 +2,7 @@
 
 import {TransactionResponse} from "../../../../generated";
 import {Box, Card, CardActionArea, Typography} from "@mui/material";
+import ColorCircle from "@/lib/components/common/ColorCircle";
 
 type TransactionListCardProps = {
   item: TransactionResponse
@@ -11,10 +12,12 @@ export default function TransactionListCard(props: TransactionListCardProps) {
   return (
     <Card key={props.item.id} variant="elevation" elevation={4}>
       <CardActionArea sx={{ padding: 2, display: "flex", alignItems: "center" }}>
-        <Typography variant="h6">
+        <ColorCircle color={props.item.category.uiColor} />
+        <Typography variant="h6" marginLeft={2}>
           {props.item.category.name}
         </Typography>
-        <Box display="flex" flex={1} justifyContent="flex-end">
+        <Box display="flex" flex={1}>
+          <Box flex={1}></Box>
           <Typography variant="h6">{props.item.amount}</Typography>
         </Box>
       </CardActionArea>

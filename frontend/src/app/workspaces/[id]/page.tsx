2a6ca0a3ba8c6b8186d2da294a52fcc3ef8f1cfa
Ruslan Omelchuk <romelchuk12@gmail.com>
2024-06-15 00:05:50 +0300

    Refactored entity page into a separate component

--- frontend/src/app/workspaces/[id]/page.tsx
+++ frontend/src/app/workspaces/[id]/page.tsx
@@ -1,36 +1,24 @@
 "use client";
 
-import {Box} from "@mui/material";
 import TransactionList from "@/lib/components/transaction/TransactionList";
 import {WorkspaceResponse} from "../../../../generated";
-import {useContext, useEffect, useState} from "react";
+import {useContext, useState} from "react";
 import {ServicesContext} from "@/lib/services/ServiceProvider";
-import {AlertContext} from "@/lib/services/AlertService";
-import {getRequestError} from "@/lib/utils/RequestUtils";
+import EntityPage from "@/lib/components/common/EntityPage";
 
 export default function WorkspacePage({ params }: { params: { id: number } }) {
   const [workspace, setWorkspace] = useState<WorkspaceResponse | null>(null);
   const { workspaceService } = useContext(ServicesContext);
-  const showAlert = useContext(AlertContext);
 
-  function fetch() {
-    const fetch = async() => {
-      const response = await workspaceService.getWorkspace({ id: params.id });
-      setWorkspace(response);
-    };
-
-    fetch().catch(e => getRequestError(e).then(m => showAlert(m, "error")))
+  async function fetch() {
+    return await workspaceService.getWorkspace({ id: params.id });
   }
-
-  useEffect(() => {
-    fetch();
-  }, []);
 
   const canCreateTransaction = workspace?.role !== undefined && workspace?.role !== "VIEWER";
 
   return (
-    <Box display="flex" flexDirection="column" height="100%" padding={2} boxSizing="border-box">
+    <EntityPage id={params.id} entity={workspace} setEntity={setWorkspace} fetch={fetch}>
       <TransactionList workspaceId={params.id} enableCreation={canCreateTransaction} />
-    </Box>
+    </EntityPage>
   );
 }

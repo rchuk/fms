67f5fd9ca542e45457aa0b4b57a854b9e8a9a418
Ruslan Omelchuk <romelchuk12@gmail.com>
2024-06-15 16:47:58 +0300

    Added logo, that redirects to dashboard on click

--- frontend/src/lib/components/common/DrawerNav.tsx
+++ frontend/src/lib/components/common/DrawerNav.tsx
@@ -1,5 +1,6 @@
-import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { Box, Button, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import { AccountCircleOutlined, CorporateFareOutlined, WorkspacesOutlined, SpaceDashboardOutlined, LoyaltyOutlined, CategoryOutlined, LogoutOutlined } from "@mui/icons-material";
+import Image from "next/image";
 import DrawerNavButton from "./DrawerNavButton";
 import { useContext } from "react";
 import { SessionServiceContext } from "@/lib/services/SessionService";
@@ -25,18 +26,11 @@ export default function DrawerNav() {
       }}
     >
       <ListItem disablePadding>
-        <ListItemText>
-          <Typography 
-            variant="h1" 
-            fontFamily={"Helvetica"}
-            sx={{
-              py: 1,
-              px: 2
-            }}
-          >
-            FMS
-          </Typography>
-        </ListItemText>
+        <Box display="flex" flexGrow={1} justifyContent="center">
+          <Button disableRipple sx={{ padding: 0 }} onClick={() => router.push("/dashboard")}>
+            <Image src="fms_logo.svg" width={200} height={80} alt="FMS" />
+          </Button>
+        </Box>
       </ListItem>
       <Divider />
       <DrawerNavButton 

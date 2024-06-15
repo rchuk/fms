fab6172e91bd984aeb44ea6bb5ef441393a281d7
W1nLin4n <grechkaartema@gmail.com>
2024-06-15 03:28:55 +0300

    Updated subscriptions page

--- frontend/src/lib/components/subscription/SubscriptionCard.tsx
+++ frontend/src/lib/components/subscription/SubscriptionCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Typography } from "@mui/material";
+import { Box, Button, Card, Typography } from "@mui/material";
 import { SubscriptionKind } from "../../../../generated";
 import { useContext } from "react";
 import { ServicesContext } from "@/lib/services/ServiceProvider";
@@ -11,22 +11,29 @@ type SubscriptionCardProps = {
   children: React.ReactNode,
   header: string,
   price: string,
-  subscriptionKind: SubscriptionKind
+  subscriptionKind: SubscriptionKind,
+  activeSubscriptionKind: SubscriptionKind | null,
+  setActiveSubscriptionKind: any
 }
 
 export default function SubscriptionCard({
   children = "",
   header = "",
   price = "",
-  subscriptionKind
+  subscriptionKind,
+  activeSubscriptionKind,
+  setActiveSubscriptionKind
 }: SubscriptionCardProps) {
   const { subscriptionService } = useContext(ServicesContext);
   const showAlert = useContext(AlertContext);
+  const subscriptionToThis = activeSubscriptionKind == subscriptionKind;
+
   function buySubscription() {
     subscriptionService
     .buySubscription( { buySubscriptionRequest: { kind: subscriptionKind } })
     .then(() => {
       showAlert(`Ви успішно купили підписку рівня ${subscriptionKind}`, "success");
+      setActiveSubscriptionKind(subscriptionKind);
     })
     .catch((e) => {
       getRequestError(e).then((m) => showAlert(m, "error"));
@@ -34,14 +41,63 @@ export default function SubscriptionCard({
   }
 
   return (
-    <>
-      <Typography variant="h2">{ header }</Typography>
-      {children}
-      <>
-        <Button onClick={buySubscription}>
-          Buy for {price}
+    <Card
+      raised={subscriptionToThis}
+      sx={{
+        p: 4,
+        borderRadius: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "30%",
+        "@media (max-width: 600px)": {
+          mt: 4,
+          width: "80%"
+        }
+      }}
+    >
+      <Typography 
+        variant="h2"
+        sx={{
+          textAlign: "center"
+        }}
+      >
+        { header }
+      </Typography>
+      <Typography 
+        variant="h6"
+        sx={{
+          mt: 4,
+          textAlign: "center"
+        }}
+      >
+        {children}
+      </Typography>
+      <Box>
+        <Button 
+          disabled={subscriptionToThis}
+          sx={{
+            mt: 4,
+            py: 2,
+            width: "100%"
+          }}
+          onClick={buySubscription}
+        >
+          <Typography 
+            variant="h6"
+          >
+            { 
+              subscriptionToThis ? 
+              (
+                <>Current plan</>
+              ) : 
+              (
+                <>Buy for {price}</>
+              ) 
+            }
+          </Typography>
         </Button>
-      </>
-    </>
+      </Box>
+    </Card>
   );
 }
--- frontend/src/lib/components/subscription/SubscriptionCardsList.tsx
+++ frontend/src/lib/components/subscription/SubscriptionCardsList.tsx
@@ -1,19 +1,87 @@
 "use client";
 
+import { Box } from "@mui/material";
 import SubscriptionCard from "./SubscriptionCard";
+import { SubscriptionKind, UserSelfResponse } from "../../../../generated";
+import { useContext, useEffect, useState } from "react";
+import { ServicesContext } from "@/lib/services/ServiceProvider";
+import { getRequestError } from "@/lib/utils/RequestUtils";
+import { AlertContext } from "@/lib/services/AlertService";
 
 export default function SubscriptionCardsList() {
+  const [activeSubscriptionKind, setActiveSubscriptionKind] = useState<SubscriptionKind | null>(null);
+  const { authService } = useContext(ServicesContext);
+  const showAlert = useContext(AlertContext);
+
+  useEffect(() => {
+    authService.getMe()
+    .then((response: UserSelfResponse) => {
+      setActiveSubscriptionKind(response.subscriptionKind);
+    })
+    .catch((e) => {
+      getRequestError(e).then(m => showAlert(m, "error"));
+    });
+  }, []);
+
   return (
-    <>
-      <SubscriptionCard header="Family plan" price="9.99$" subscriptionKind="FAMILY">
-        Buy family plan
-      </SubscriptionCard>
-      <SubscriptionCard header="Business plan" price="29.99$" subscriptionKind="BUSINESS">
-        Buy business plan
-      </SubscriptionCard>
-      <SubscriptionCard header="Business unlimited plan" price="49.99$" subscriptionKind="BUSINESS_UNLIMITED">
-        Buy business unlimited plan
-      </SubscriptionCard>
-    </>
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        "@media (max-width: 600px)": {
+          height: "auto",
+        }
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "start",
+          "@media (max-width: 600px)": {
+            height: "auto",
+            flexDirection: "column",
+            alignItems: "center"
+          }
+        }}
+      >
+        <SubscriptionCard 
+          header="Family" 
+          price="9.99$" 
+          subscriptionKind="FAMILY"
+          activeSubscriptionKind={activeSubscriptionKind}
+          setActiveSubscriptionKind={setActiveSubscriptionKind}
+        >
+          Want to try out the power of shared workspaces, but do not want to spend too much?
+          Then this plan is just for you. With this plan you can create a shared workspace for 4
+          users. It is a perfect option for families and small teams.
+        </SubscriptionCard>
+        <SubscriptionCard 
+          header="Business" 
+          price="29.99$" 
+          subscriptionKind="BUSINESS"
+          activeSubscriptionKind={activeSubscriptionKind}
+          setActiveSubscriptionKind={setActiveSubscriptionKind}
+        >
+          You are running a business and 4 users in a shared workspace is not enough for you? Then
+          try out this plan. Remove the limit of 4 users in a workspace and enjoy the flexibility
+          of an unlimited shared workspace right now.
+        </SubscriptionCard>
+        <SubscriptionCard 
+          header="Business Unlimited" 
+          price="49.99$" 
+          subscriptionKind="BUSINESS_UNLIMITED"
+          activeSubscriptionKind={activeSubscriptionKind}
+          setActiveSubscriptionKind={setActiveSubscriptionKind}
+        >
+          Single shared workspace is not enough for you? Then you definitely should buy this plan. 
+          With it, you can have unlimited amout of shared workspaces and organizations.
+        </SubscriptionCard>
+      </Box>
+    </Box>
   )
 }
